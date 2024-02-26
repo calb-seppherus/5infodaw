afee5b3bca44a024a644a3dd4a673f13c2cbb1bb
calb-seppherus <gabriel.freitas2006@proton.me>
2024-02-26 11:09:40 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res)=>{
+    res.send('Hello World!')
+})
+
+app.get('/gabriel', (req,res)=>{
+    res.send('Hello Gabriel!')
+})
+
+app.get('/:nome', (req,res)=>{
+    res.send('Hello '+req.params.nome+'!')
+})
+
+app.get('/:nome/:sobrenome', (req,res)=>{
+    res.send('Hello '+req.params.nome+' '+req.params.sobrenome+'!')
+})
+
+app.get('/soma/:numero1/:numero2', (req,res)=>{
+    const n1 = parseInt(req.params.numero1)
+    const n2 = parseInt(req.params.numero2)
+    const resposta = (n1 + n2)
+    res.send('O resulatdo da soma é '+resposta)
+})
+
+app.get('/somad/:numero1/:numero2', (req,res)=>{
+    const n1 = parseInt(req.params.numero1)
+    const n2 = parseInt(req.params.numero2)
+    const resposta = (n1 + n2) * 2
+    res.send('O resulatdo da soma é '+resposta)
+})
+
+app.listen(port)
