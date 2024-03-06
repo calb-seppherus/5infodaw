624433eba463afd22bf71cc67695797d8ec8d531
calb-seppherus <gabriel.freitas2006@proton.me>
2024-03-06 09:19:47 -0300

    enviado todas as funções para controller.js

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,3 +2,66 @@ export function abreindex (req,res)
 {
     res.render('index')
 }
+
+
+export function calculo (req,res)
+{
+    res.render('calculo')
+}
+
+export function calcular (req,res)
+{
+    const n1 = parseInt(req.body.numero1)
+    const n2 = parseInt(req.body.numero2)
+    const resposta = n1 + n2
+    res.render('calculo',{resposta:resposta})
+    //res.send('Resultado: '+resposta)
+}
+
+export function paragrafo (req,res)
+{
+    res.render('paragrafo')
+}
+
+export function paragrafar (req,res)
+{
+    const maxparag = parseInt(req.body.parag)
+    const coluna = parseInt(req.body.coluna)
+    res.render('paragrafo',{maxparag:maxparag,coluna:coluna})
+}
+
+export function gabriel (req,res)
+{
+    res.send('Hello Gabriel!')
+}
+
+export function nome (req,res)
+{
+    res.send('Hello '+req.params.nome+'!')
+}
+
+export function nomesobrenome (req,res)
+{
+    res.send('Hello '+req.params.nome+' '+req.params.sobrenome+'!')
+}
+
+export function soma (req,res)
+{
+    const n1 = parseInt(req.params.numero1)
+    const n2 = parseInt(req.params.numero2)
+    const resposta = (n1 + n2)
+    res.send('O resulatdo da soma é '+resposta)
+}
+
+export function somadobro (req,res)
+{
+    const n1 = parseInt(req.params.numero1)
+    const n2 = parseInt(req.params.numero2)
+    const resposta = (n1 + n2) * 2
+    res.send('O resulatdo da soma é '+resposta)
+}
+
+export function pesquisar (req,res)
+{
+    res.send('dados recebidos: '+req.body.nome)
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,73 +1,28 @@
 import express from 'express';
 export const router = express.Router();
+import {abreindex,calculo,calcular,paragrafo,paragrafar,gabriel,nome,nomesobrenome,soma,somadobro,pesquisar} from '../controllers/controllers.js'
 
-router.get('/', (req,res)=>
-{
-    res.render('index')
-})
+router.get('/',)
 
-//rota para abrir a pagina calculo.ejs
-router.get('/calculo',(req,res)=>
-{
-    res.render('calculo')
-})
+router.get('/calculo',)
 
 //rota para receber os dados, fazer o calculo e mostrar o resultado final
-router.post('/calcular',(req,res)=>
-{
-    const n1 = parseInt(req.body.numero1)
-    const n2 = parseInt(req.body.numero2)
-    const resposta = n1 + n2
-    res.render('calculo',{resposta:resposta})
-    //res.send('Resultado: '+resposta)
-}) 
+router.post('/calcular',) 
 
-router.get('/paragrafo',(req,res)=>
-{
-    res.render('paragrafo')
-})
+router.get('/paragrafo',)
 
-router.post('/paragrafar',(req,res)=>
-{
-    const maxparag = parseInt(req.body.parag)
-    const coluna = parseInt(req.body.coluna)
-    res.render('paragrafo',{maxparag:maxparag,coluna:coluna})
-})
+router.post('/paragrafar',)
 
-router.get('/gabriel', (req,res)=>
-{
-    res.send('Hello Gabriel!')
-})
+router.get('/gabriel',)
 
-router.get('/:nome', (req,res)=>
-{
-    res.send('Hello '+req.params.nome+'!')
-})
+router.get('/:nome',)
 
-router.get('/:nome/:sobrenome', (req,res)=>
-{
-    res.send('Hello '+req.params.nome+' '+req.params.sobrenome+'!')
-})
+router.get('/:nome/:sobrenome', )
 
-router.get('/soma/:numero1/:numero2', (req,res)=>
-{
-    const n1 = parseInt(req.params.numero1)
-    const n2 = parseInt(req.params.numero2)
-    const resposta = (n1 + n2)
-    res.send('O resulatdo da soma é '+resposta)
-})
+router.get('/soma/:numero1/:numero2', )
 
-router.get('/somadobro/:numero1/:numero2', (req,res)=>
-{
-    const n1 = parseInt(req.params.numero1)
-    const n2 = parseInt(req.params.numero2)
-    const resposta = (n1 + n2) * 2
-    res.send('O resulatdo da soma é '+resposta)
-})
+router.get('/somadobro/:numero1/:numero2',)
 
-router.post('/pesquisar', (req,res) =>
-{
-    res.send('dados recebidos: '+req.body.nome)
-})
+router.post('/pesquisar',)
 
 export default router;
